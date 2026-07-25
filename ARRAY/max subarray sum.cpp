e87fe73ceb8105dad5ed049af33402b958109528
Aashi <aashi007johari@gmail.com>
2026-07-25 13:24:42 +0530

    Max subarray sum in ascending order

---
+++ ARRAY/max subarray sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+       int sum=nums[0];
+       int ans = sum;
+       int n= nums.size();
+for (int i =1; i<n; i++){
+
+
+if(nums[i]>nums[i-1]){
+    sum+=nums[i];
+}else{
+sum=nums[i];}
+ans=max(ans,sum);
+}
+
+
+       return ans;
+    }  
+};
