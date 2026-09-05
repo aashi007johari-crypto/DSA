e8c63f518888b99dc2222bc48f48abf9c78cc2ee
Aashi <aashi007johari@gmail.com>
2026-09-05 17:49:28 +0530

    Smallest stable index

---
+++ ARRAY/smallest stable index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int score=0;
+
+        for(int i=0;i<nums.size();i++){
+         score=*max_element(nums.begin(),nums.begin()+i+1)-*min_element(nums.begin()+i,nums.end());
+
+         if(score<=k){
+           return i;
+         }
+         
+        }
+        return-1;
+    }
+};
