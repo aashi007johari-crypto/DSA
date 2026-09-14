8eb00c37f0ab01f38231374580bc312a3fd460bc
Aashi <aashi007johari@gmail.com>
2026-09-14 14:19:40 +0530

    Container with most water

---
+++ ARRAY/Two Pointers/mostwater.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+     int l=0;
+     int r=height.size()-1;
+     int maxW=0;
+
+     while(l<r){
+        int h=min(height[l],height[r]);
+        int width=r-l;
+        int area=h*width;
+        maxW=max(maxW,area);
+
+        if(height[l]<height[r]){
+            l++;
+        }else
+        r--;
+     }
+        return maxW;
+    }
+};
