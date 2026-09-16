9abf960ff8120b9408300dace33301548fd1b245
Aashi <aashi007johari@gmail.com>
2026-09-16 15:55:12 +0530

    3sum closest

---
+++ ARRAY/Two Pointers/3sum closest.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+      
+       int ans=nums[0]+nums[1]+nums[2];;
+       sort(nums.begin(),nums.end());
+       for(int i=0;i<nums.size()-2;i++) {
+         int l=i+1;
+       int r=nums.size()-1;
+       while(l<r){
+        int sum=nums[l]+nums[r]+nums[i];
+
+if(abs(sum - target) < abs(ans - target)) {
+    ans = sum;
+}
+        if(sum>target){
+            r--;
+        }else if(sum<target){
+            l++;
+        }else{
+           return ans;
+        }
+       }
+       }
+       return ans;
+    }
+};
